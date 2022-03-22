f0a9a744c76ce440bd90c9ad3cc406eecd5d36d8
dimomarg <dimomarg@gmail.com>
2022-03-22 20:49:10 +0200

    Initial Commit

---
+++ fenSet.js
@@ -0,0 +1,68 @@
+const isPiece = function(input){
+    input = input.toUpperCase();
+    if (input === "K" ||
+        input === "Q" ||
+        input === "B" ||
+        input === "N" ||
+        input === "R" ||
+        input === "P"){
+            return true;
+        }
+    return false;
+}
+
+const isGap = function(input){
+    if (input >"0" && input <"9"){
+        return true;
+    }
+    return false;
+}
+
+let fenSet = {
+    symbols: {
+        "K":"Kk",
+        "Q":"Qq",
+        "B":"Bb",
+        "N":"Nn",
+        "R":"Rr",
+        "P":"Pp",
+        "k":"Ll",
+        "q":"Ww",
+        "b":"Vv",
+        "n":"Mm",
+        "r":"Tt",
+        "p":"Oo",
+        empty:"zx"
+    },
+    
+    convert: function(fenString){
+        let row = 0;
+        let square = 0;
+        let output = "";
+        let currSquare = "";
+        let i = 0;
+        while(square<64){
+            currSquare = fenString[i];
+            if (isPiece(currSquare)){
+                output += this.symbols[currSquare][(square+row)%2];
+                ++square;
+                ++i;
+            }
+            else if (isGap(currSquare)){
+                while (currSquare > 0){
+                    output += this.symbols.empty[(square+row)%2];
+                    ++square;
+                    --currSquare;
+                }
+                ++i;
+            }
+            else if (currSquare === "/"){
+                output += "\n";
+                ++i;
+                ++row;
+            }
+        }
+        return(output);
+    }
+}
+
